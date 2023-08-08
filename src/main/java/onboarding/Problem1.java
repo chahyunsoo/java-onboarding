26e91125fdf6a7e8b2acfd0b9c454f1fe05786b2
chahyunsoo <chahs1012@naver.com>
2023-08-09 00:49:00 +0900

    refactor(Problem1) : 전체적인 1차 리팩토링 완료
    전반적으로 기본형 이었던 매개변수를 Wrapper 인스턴스로 변경
    누가 이겼는지 한눈에 알기 쉽게 return값을 상수로 설정
    예외가 발생할 수 있는 로직을 따로 메소드로 만들어 예외를 검증

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,73 +3,63 @@ package onboarding;
 import java.util.*;
 
 class Problem1 {
+    public static final int DRAW = 0;
+    public static final int POBI_WIN = 1;
+    public static final int POBI_LOSE = 2;
+    public static final int EXCEPTIONS = -1;
+
     static class PageGapException extends Exception {
         int pageGapException() {
-            return -1;
+            return EXCEPTIONS;
         }
     }
 
     static class OverPageException extends Exception {
         int overPageException() {
-            return -1;
+            return EXCEPTIONS;
         }
     }
 
-//    페이지 초기화: 왼쪽 페이지, 오른쪽 페이지 각각 랜덤으로
-//    private static List<Integer> initPages() throws PageException {
-//        Random random = new Random();
-//        int leftPage = random.nextInt(400) + 1;
-//        int rightPage = random.nextInt(400) + 1;
-//        if (rightPage - leftPage != 1) {
-//            throw new PageException();
-//        }
-//        System.out.println("왼쪽 페이지= " + leftPage + "오른쪽 페이지= " + rightPage);
-//        List<Integer> list = Arrays.asList(leftPage, rightPage);
-//        return list;
-//    }
 
     public static int returnResultNumber(List<Integer> pobi, List<Integer> crong) throws PageGapException, OverPageException {
-        int[] pobiPageArray = new int[2];
-        int[] crongPageArray = new int[2];
-        int pobiIndex = 0;
-        int crongIndex = 0;
+        int pobiMaxResult = comparePage(pobi.get(0),pobi.get(1));
+        int crongMaxResult = comparePage(crong.get(0),crong.get(1));
 
-        /*
-         * 페이지가 1페이지 미만이거나 400페이지 초과일때 예외 발생
-         *
-         * 오른쪽 페이지에서 왼쪽 페이지를 뺐을때 무조건 1이어야
-         * 연속된 페이지라고 볼 수 있으므로 1이 아니면 예외 발생
-         * */
-        for (Integer integer : pobi) {
-            if (integer.intValue() < 1 || integer > 400) {
-                throw new OverPageException();
-            }
-            pobiPageArray[pobiIndex++] = integer.intValue();
-
-        }
-        if (pobiPageArray[1] - pobiPageArray[0] != 1) {
-            throw new PageGapException();
-        }
-        for (Integer integer : crong) {
-            if (integer.intValue() < 1 || integer > 400) {
-                throw new OverPageException();
-            }
-            crongPageArray[crongIndex++] = integer.intValue();
-        }
-        if (crongPageArray[1] - crongPageArray[0] != 1) {
-            throw new PageGapException();
-        }
-
-        int pobiMaxResult = comparePage(pobiPageArray);
-        int crongMaxResult = comparePage(crongPageArray);
+        validateException(pobi, crong);
 
         if (pobiMaxResult > crongMaxResult) {
-            return 1;
-        } else if (pobiMaxResult < crongMaxResult) {
-            return 2;
-        } else {
-            return 0;
+            return POBI_WIN;
         }
+        if (pobiMaxResult < crongMaxResult) {
+            return POBI_LOSE;
+        }
+        if(pobiMaxResult == crongMaxResult) {
+            return DRAW;
+        }
+        return -2;
+    }
+
+    private static void validateException(List<Integer> pobi,List<Integer> crong) throws PageGapException,OverPageException{
+        if (checkPageGapException(pobi.get(1), pobi.get(0)) == true) {
+            throw new PageGapException();
+        }
+        if (checkOverPageException(pobi) == true || checkOverPageException(crong) == true) {
+            throw new OverPageException();
+        }
+    }
+
+    private static boolean checkPageGapException(int rightPageIndex,int leftPageIndex) {
+        boolean pageGapIsNotOne = (rightPageIndex - leftPageIndex != 1);
+        return pageGapIsNotOne;
+    }
+
+    private static boolean checkOverPageException(List<Integer> list) {
+        boolean pageIsOverPage=false; //OverPage가 아니다가 기본값
+        for (Integer integer : list) {
+            if (integer.intValue() < 1 || integer.intValue() > 400) {
+                pageIsOverPage = true;
+            }}
+        return pageIsOverPage;
     }
 
     public static int solution(List<Integer> pobi, List<Integer> crong) {
@@ -83,18 +73,17 @@ class Problem1 {
         }
     }
 
-    private static int comparePage(int[] pageArray) {
-        int maxResult = 0;
-        int leftPageSum=CalculatePageSum(pageArray[0]);
-        int rightPageSum=CalculatePageSum(pageArray[1]);
-        int leftPageMultiply = CalculatePageMultiply(pageArray[0]);
-        int rightPageMultiply = CalculatePageMultiply(pageArray[1]);
-        int[] calculatePageSumAndMultiply = new int[]{leftPageSum, rightPageSum, leftPageMultiply, rightPageMultiply};
+    private static int comparePage(Integer getLeftPage,Integer getRightPage) {
+        Integer leftPageSum=CalculatePageSum(getLeftPage);
+        Integer rightPageSum=CalculatePageSum(getRightPage);
+        Integer leftPageMultiply = CalculatePageMultiply(getLeftPage);
+        Integer rightPageMultiply = CalculatePageMultiply(getRightPage);
+        Integer[] calculatePageSumAndMultiply = new Integer[]{leftPageSum, rightPageSum, leftPageMultiply, rightPageMultiply};
 
-//        int maxResult = Arrays.stream(calculatePageSumAndMultiply).max().getAsInt();
-        for (int value : calculatePageSumAndMultiply) {
-            if (value > maxResult) {
-                maxResult = value;
+        int maxResult=0;
+        for (Integer integer : calculatePageSumAndMultiply) {
+            if (integer.intValue() > maxResult) {
+                maxResult = integer;
             }
         }
         return maxResult;
