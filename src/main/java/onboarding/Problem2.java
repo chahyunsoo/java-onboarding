9c32142c520f7f4d15e403cc2ae7a3ff41f19f81
chahyunsoo <chahs1012@naver.com>
2023-09-14 23:11:03 +0900

    feat(Problem2) : 스택의 문자들 문자열로 바꾸는 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -7,25 +7,27 @@ public class Problem2 {
     static final int MIN_LENGTH = 1;
     static final char SMALL_LETTER_FIRST = 'a';
     static final char SMALL_LETTER_LAST = 'z';
-    public static Stack<Character> stack = new Stack<>();
-    public static StringBuilder sb = new StringBuilder();
-    public static String answer = "";
+    public static Stack<Character> stack;
+    public static String answer="";
+
+    public static void main(String[] args) {
+
+    }
 
     public static String solution(String cryptogram) {
         try {
             validateAllConstraints(cryptogram);
-            checkAndDeleteDuplicatedLetters(cryptogram);
-
-
-
         } catch (Exception e) {
             e.printStackTrace();
         }
+        Stack<Character> characters = checkAndDeleteDuplicatedLetters(cryptogram);
+        String answer = stackToStringBuilder(characters);
+
         return answer;
     }
 
     /**
-     * 제한사항 한번에 검증
+     * 제한 사항 한번에 검증
      * @param cryptogram
      */
     public static void validateAllConstraints(String cryptogram) {
@@ -38,7 +40,7 @@ public class Problem2 {
      * @param cryptogram
      */
     public static void validateLength(String cryptogram) {
-        if (cryptogram.length() < MIN_LENGTH && cryptogram.length() > MAX_LENGTH) {
+        if (cryptogram.length() < MIN_LENGTH || cryptogram.length() > MAX_LENGTH) {
             throw new IllegalArgumentException();
         }
     }
@@ -59,7 +61,7 @@ public class Problem2 {
      * @param letter
      */
     private static void checkSmallLetter(char letter) {
-        if (letter < SMALL_LETTER_FIRST && letter > SMALL_LETTER_LAST) {
+        if (letter < SMALL_LETTER_FIRST || letter > SMALL_LETTER_LAST) {
             throw new IllegalArgumentException();
         }
     }
@@ -68,11 +70,13 @@ public class Problem2 {
      * cryptogram 문자 배열을 돌면서 중복된 문자 제거하는 기능
      * @param cryptogram
      */
-    public static void checkAndDeleteDuplicatedLetters(String cryptogram) {
+    public static Stack<Character> checkAndDeleteDuplicatedLetters(String cryptogram) {
+        stack = new Stack<>();
         char[] letters = cryptogram.toCharArray();
         for (char letter : letters) {
             deleteDuplicatedLetter(letter);
         }
+        return stack;
     }
 
     /**
@@ -86,6 +90,14 @@ public class Problem2 {
             return;
         }
         stack.push(letter);
+    }
+
+    public static String stackToStringBuilder(Stack<Character> stack) {
+        StringBuilder sb = new StringBuilder();
+        for (char letter : stack) {
+            sb.append(letter);
+        }
+        return sb.toString();
     }
 
 
